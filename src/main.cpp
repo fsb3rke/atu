264674703ec0a2bb5fb6738f8f2b356ff2426178
fsb3rke <berkeeavcii@gmail.com>
2023-11-27 18:56:42 +0300

    feat: added new features in main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include "../header/commands.h"
 #include "../header/handlerer.h"
+#include "../header/shw.h"
 
 int main()
 {
+    Shw shw;
     CommandHandlerer commandHandlerer;
+
+
+    shw.sendMessages();
 
     commandHandlerer.executeCommands(
         COMMS
